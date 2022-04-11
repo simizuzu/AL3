e443ea476d037eca766b13b9f5e83c55d88e700e
ShoShimizu <k021g1379@g.neec.ac.jp>
2022-04-12 00:54:22 +0900

    default

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow();
+	win->CreateGameWindow("LE2C_17_シミズ_ショウ_AL3");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
